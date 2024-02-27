0937428eac8b551da73a58565c632604efafeb97
Alexandre <60877715+reisemh3@users.noreply.github.com>
2024-02-27 17:47:18 +0100

    Style card

--- front/src/components/bookcard/BookCard.tsx
+++ front/src/components/bookcard/BookCard.tsx
@@ -1,17 +1,28 @@
-
 import './BookCard.css';
-export default BookCard;
 
-function BookCard({ title, author }: { title: string, author: string }) {
+interface BookCardProps {
+    title: string;
+    author: string;
+    coverImage: string;
+  }
+  
+  const BookCard: React.FC<BookCardProps> = ({ title, author, coverImage }) => {
 
     return (
-        <div className='card w-80'>
-            <div className='info'>
-                <h3 className='title'>{title}</h3>
-                <p className='author'>Auteur : {author}</p>
+        <>
+            <div className="card">
+                <div className="card-border-top">
+                </div>
+                <div className="img">
+                    <img src={coverImage} alt=""className='image' />
+                </div>
+                <span><h3 className='title'>{title}</h3></span>
+                <p className="job author">Auteur : {author}</p>
+                <button> Lire
+                </button>
             </div>
-            <img src="" alt={title} className='image' />
-        </div>
+        </>
     );
 }
 
+export default BookCard;
