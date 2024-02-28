38a9b246bf4bc69dd3ce944776da1381855ec231
Alexandre <60877715+reisemh3@users.noreply.github.com>
2024-02-28 15:41:09 +0100

    Commit recherche
    Modification des recherches via Url, ajout de spinner chargement de JSON et amélioration de l'affichage des livres à la taille de fenetre

--- front/src/components/bookcard/BookCard.tsx
+++ front/src/components/bookcard/BookCard.tsx
@@ -1,25 +1,36 @@
+// import React from 'react';
+import React from 'react';
+import image1 from './../../img/bookaleatoire/frappe.gif';
+import image2 from './../../img/bookaleatoire/oh.png';
+import image3 from './../../img/bookaleatoire/poop.png';
 import './BookCard.css';
 
 interface BookCardProps {
+    id: number; // Ajoutez cette ligne
     title: string;
     author: string;
-    coverImage: string;
+    coverImage?: string; // Faites de coverImage une propriété optionnelle
+    onRead: () => void; // Ajoutez cette ligne
   }
   
-  const BookCard: React.FC<BookCardProps> = ({ title, author, coverImage }) => {
+  const BookCard: React.FC<BookCardProps> = ({ id, title, author, coverImage, onRead }) => {
+    const images = [image1, image2, image3]; // Remplacez par les chemins de vos images
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const defaultImage = images[randomIndex];
+    
+    const displayTitle = title.length > 50 ? title.substring(0, 50) + '...' : title;
+    const [isHovered, setIsHovered] = React.useState(false);
 
     return (
         <>
-            <div className="card">
-                <div className="card-border-top">
-                </div>
+            <div key={id} className="card" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} title={`Titre : ${title}`}>
+                <div className="card-border-top"></div>
                 <div className="img">
-                    <img src={coverImage} alt=""className='image' />
+                    <img src={coverImage ? coverImage : defaultImage} alt="" className='image' />
                 </div>
-                <span><h3 className='title'>{title}</h3></span>
-                <p className="job author">Auteur : {author}</p>
-                <button> Lire
-                </button>
+                <span><h3 className='title'>{isHovered ? title : (title.length > 10 ? title.substring(0, 10) + '...' : title)}</h3></span>
+                <p className="job author">Auteur : {isHovered ? author : (author.length > 15 ? author.substring(0, 15) + '...' : author)}</p>
+                <button onClick={onRead}> Lire </button>
             </div>
         </>
     );
